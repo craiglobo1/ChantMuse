aa113e245bd1d057542ab332cceadd6d25d447f4
craiglobo1 <craiglobo1@gmail.com>
2025-06-12 17:15:43 -0600

    fixed unused variables

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+
 import './App.css'
 import GabcEditor from './components/gabceditor'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
   return (
     <>
